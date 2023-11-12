834156feec37a4cbfdf09364b6c69866177bc4ec
Pablo-Damian <132961771+Pablo-Damian@users.noreply.github.com>
2023-11-12 15:33:40 -0300

    Add files via upload

---
+++ BBDDpersonas(backup).sql
@@ -0,0 +1,73 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 6.0.0-dev
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 192.168.30.23
+-- Tiempo de generación: 29-10-2023 a las 23:42:31
+-- Versión del servidor: 8.0.18
+-- Versión de PHP: 8.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `BBDDpersonas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE `personas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `apellido` varchar(40) NOT NULL,
+  `edad` tinyint(2) NOT NULL,
+  `fecha_creación` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `provincia` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `edad`, `fecha_creación`, `provincia`) VALUES
+(1, 'Juan', 'Pérez', 25, '2023-10-30 00:40:46', 'Buenos Aires'),
+(2, 'María', 'López', 20, '2023-10-30 00:40:46', 'Córdoba'),
+(3, 'Pedro', 'García', 30, '2023-10-30 00:40:46', 'Santa Fe'),
+(4, 'Ana', 'González', 35, '2023-10-30 00:40:46', 'Entre Ríos'),
+(5, 'José', 'Martínez', 40, '2023-10-30 00:40:46', 'Corrientes');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `personas`
+--
+ALTER TABLE `personas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ BBDDpersonas.sql
@@ -0,0 +1,27 @@
+﻿CREATE DATABASE BBDDpersonas;
+
+USE BBDDpersonas;
+
+CREATE TABLE IF NOT EXISTS personas (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(40) NOT NULL,
+  apellido VARCHAR(40) NOT NULL,
+  edad TINYINT(2) NOT NULL,
+  fecha_creación DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  provincia VARCHAR(30) NOT NULL
+);
+
+INSERT INTO personas (nombre, apellido, edad, provincia) VALUES
+('Juan', 'Pérez', 25, 'Buenos Aires'),
+('María', 'López', 20, 'Córdoba'),
+('Pedro', 'García', 30, 'Santa Fe'),
+('Ana', 'González', 35, 'Entre Ríos'),
+('José', 'Martínez', 40, 'Corrientes');
+
+-- Verificación de datos.
+SELECT * FROM personas;
+-- SELECT * FROM personas LIMIT 5;
+
+-- SELECT * FROM `personas` WHERE 1
+
+-- BACKUP TABLE personas TO '/home/user/backups/personas.sql';
